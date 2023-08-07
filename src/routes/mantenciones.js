258e159e8b9d29f8f3f5afb3cb3432e53ce5dd04
Oscar Caro <55809504+Oskhar-ogg@users.noreply.github.com>
2023-08-06 22:03:25 -0400

    update

--- src/routes/mantenciones.js
+++ src/routes/mantenciones.js
@@ -3,30 +3,10 @@ import { getMantencionesCaldera, getMantencionesCalefont } from "../controllers/
 
 const router = Router();
 
-/**
- * @swagger
- * tags:
- *      name: Mantenciones
- *          description: Mantenciones API
- * */
 
 router.get('/mantenciones/caldera', getMantencionesCaldera);
 
-/**
- * @swagger
- * /mantenciones/caldera:
- *      get:
- *          summary: Carga la lista de mantenciones de caldera.
- *          tags: [Mantenciones]
- * */
 
 router.get('/mantenciones/calefont', getMantencionesCalefont);
 
-/**
- * @swagger
- * /mantenciones/calefont:
- *      get:
- *          summary: Carga la lista de mantenciones de calefont.
- *          tags: [Mantenciones]
- * */
 export default router;
