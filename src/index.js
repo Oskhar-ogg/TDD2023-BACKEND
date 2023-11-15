aabe1bc6f0660cb39e4d45f45bb54ba6be6d0c65
Oscar Caro <55809504+Oskhar-ogg@users.noreply.github.com>
2023-11-15 14:44:15 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ app.use(bodyParser.json());
 
 // Resto del código del servidor...
 
-app.listen(3000, () => {
-  console.log('Servidor listo y escuchando en el puerto 3000');
+app.listen(5000, () => {
+  console.log('Servidor listo y escuchando en el puerto 5000');
 });
